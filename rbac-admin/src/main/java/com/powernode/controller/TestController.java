ad3ff55e69058a6a9792182238c3a4b3a0e9c8da
lzj <lzj_0304@163.com>
2023-03-24 15:21:45 +0800

    v_1.1 分支添加文件 testController

---
+++ rbac-admin/src/main/java/com/powernode/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.powernode.controller;
+
+/**
+ * @Author: Mr.T
+ * @Date: 2023/3/24
+ */
+public class TestController {
+
+    public void test(){
+        System.out.println("TestController.test....");
+    }
+}
